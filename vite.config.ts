7aeabb117dcc8159a6a3e9dc2295e05f9a4b9647
simon123h <simon123h@users.noreply.github.com>
2026-05-20 23:36:34 +0200

    feat: ready for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // For GitLab Pages deployment, set base to your repo name if needed:
-  // base: '/fairshare/',
+  base: '/fairshare/',
 })
